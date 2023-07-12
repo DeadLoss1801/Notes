5add4590b64c8751271883ee508046c57112d056
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2023-07-12 12:59:19 +0530

    By Size

--- DSA/DisjointSet/UnionBYRank.cpp
+++ DSA/DisjointSet/UnionBYRank.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 class DisjointSet
 {
-    vector<int> size, parent;
+    vector<int> rank, parent;
 
 public:
     DisjointSet(int n)
     {
-        size.resize(n + 1, 0);
+        rank.resize(n + 1, 0);
         parent.resize(n + 1);
 
         for (int i = 0; i <= n; i++)
@@ -23,7 +23,7 @@ public:
         return parent[node] = findUPar(parent[node]);
     }
 
-    void unionBysize(int u, int v)
+    void unionByRank(int u, int v)
     {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
@@ -31,15 +31,18 @@ public:
         if (ulp_u == ulp_v)
             return;
 
-        if (size[ulp_u] < size[ulp_v])
+        if (rank[ulp_u] < rank[ulp_v])
         {
             parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u]
+        }
+        else if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
         }
         else
         {
             parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v]
+            rank[ulp_u]++;
         }
     }
 };
