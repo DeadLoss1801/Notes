756458cb27e128089377012a59c8cebc1603050d
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2023-07-26 14:19:46 +0530

    K

--- DSA/Dp/18_dp.py
+++ DSA/Dp/18_dp.py
@@ -1,29 +1,36 @@
-# Subset sum equal to target (+ways)
+# Subset sum equal to target (duplicate)
 
 def solve(n, k, arr):
-    dp = [[0 for j in range(k+1)] for i in range(n)]
+    dp = [[0]*(k+1)for i in range(n)]
     for i in range(n):
         dp[i][0] = 1
 
-    if arr[0] <= k:
+    if arr[0]  == 0 :
+        dp[0][0] = 2
+    else :
+        dp[0][0] = 1   
+
+    if arr[0]!=0 and arr[0] <= k:
         dp[0][arr[0]] = 1
 
     for ind in range(1, n):
-        for target in range(1, k+1):
+        for target in range(0, k+1):
             notTaken = dp[ind-1][target]
             Taken = 0
             if arr[ind] <= target:
                 Taken = dp[ind-1][target - arr[ind]]
             dp[ind][target] = Taken + notTaken
+            # print(Taken  , notTaken)
+        # print(dp[ind][k])
+    
     return dp[n-1][k]
 
 
 def main():
-    arr = [1, 2, 2, 3]
-    k = 3
+    arr = [0, 0, 1,2 ]
+    k = 1
     n = len(arr)
-
-    print(solve(n, k, arr))
+    print(solve(n, sum(arr), arr))
 
 
 if __name__ == "__main__":
