f051efa3b58353762dec122de141584379f78da3
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2024-03-07 01:03:20 +0530

    template.cpp

--- template.cpp
+++ template.cpp
@@ -1,28 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fio                       \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
+
 #define int long long int
 #define vi vector<int>
-#define w(x)  \
-    int x;    \
-    cin >> x; \
-    while (x--)
 #define pb push_back
 
 int32_t main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    fio
-    w(t)
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
     {
-        int n;
-        cin >> n;
-        cout << (n - 1) * (n - 2) + 1 << "\n";
+        int n, p, k;
+        cin >> n >> p >> k;
+        int m = p % k;
+        int d = n / k;
+        int mx = n % k;
+
+        int total = 0;
+        if (mx >= m && m != 0)
+        {
+            total += (m - 1) * (d + 1);
+            total += (p / k);
+        }
+        else if (m == 0)
+        {
+            total = p / k;
+        }
+        else
+        {
+            total += (mx) * (d + 1);
+            total += (m - mx) * d;
+            total += (p / k);
+        }
+        cout << total + 1 << endl;
     }
 }
+
+// 0 5 10 1 6 2 7  3
